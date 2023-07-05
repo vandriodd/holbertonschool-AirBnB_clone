354321cd72c5a3617d66f03cb16c9dfd75ac124d
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-07-05 13:02:37 -0700

    Adding test_models directory

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    test_base_model module
+"""
+
+import unittest
+from models.base_model import BaseModel
+
+class test_base_model(unittest.TestCase):
+    """ Tests BaseModel class """
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
